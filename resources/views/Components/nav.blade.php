5d98a94138db59c1e8d1c47de4be6d55b0f1483f
Hasanelhaj <sehasanelhaj@gmail.com>
2024-09-18 16:38:09 -0700

    first

---
+++ resources/views/Components/nav.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+        <nav>
+           <x-nav-link href="/">Home</x-nav-link>
+           <x-nav-link href="/about">About</x-nav-link>
+           <x-nav-link href="/contact">Contact</x-nav-link>
+        </nav>
+</html>
---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello this is the About Page!</h1>
+</x-layout>
