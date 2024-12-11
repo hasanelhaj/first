ac1e07dc811c23d3ab9bcae7f370dc6f7e768e00
Hasan El-haj <70118069+hasanelhaj@users.noreply.github.com>
2024-12-10 21:33:45 -0800

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
     <h1>Hello this is the About Page!</h1>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
     <h1>Hello this is the Contact Page!</h1>
 </x-layout>
